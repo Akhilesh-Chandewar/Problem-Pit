c4d3bbe038ee2528a01346f4ca1f3933ac8ca0f4
Akhil Chandewar <akhil.chandewar00@gmail.com>
2025-12-30 23:03:36 +0530

    css fixes and theme provider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,10 @@
-// import { onBoardUser } from "@/module/auth/action";
 import { UserButton } from "@clerk/nextjs";
 
 export default async function Home() {
-  // await onBoardUser();
   return (
-    <div className="min-h-screen bg-linear-to-b from-blue-50 to-indigo-100 flex items-center justify-center">
+    <div className="min-h-screen bg-linear-to-b flex items-center justify-center">
       <div className="text-center">
         <UserButton />
-        <h1 className="mt-6 text-4xl font-bold text-gray-800">Welcome to the App!</h1>
-        <p className="mt-4 text-lg text-gray-600">
-          This is your personalized dashboard.
-        </p>
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/themeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,21 +28,26 @@ export const metadata: Metadata = {
   ],
 };
 
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ClerkProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+          >
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
